80a51598023e0fb03327197f29f6cc387d1ceb02
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-05 01:54:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,12 +12,13 @@ class Anagram
     # (@word.match(%w(array)))
     split_word = @word.split("").sort # ["a", "d", "e", "i", "p", "r"]
     # split_element = []
-    array.collect do |element|
-      split_element = element.split("").sort
-      if split_element == split_word
-        return element
-      end
-    end
+    # array.collect do |element|
+    #   split_element = element.split("").sort
+    #   if split_element == split_word
+    #     return element
+    #   end
+    # end
+    array.select {|i| %w(i).sort == split_word}
     binding.pry
   end
 
